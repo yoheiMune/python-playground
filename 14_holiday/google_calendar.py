cfb76e6655ca55194c9746ddbc3640fcbc52b2aa
Yohei Munesada <y.munesada@gmail.com>
2016-04-30 22:43:49 +0900

    refactoring

--- 14_holiday/google_calendar.py
+++ 14_holiday/google_calendar.py
@@ -1,36 +1,18 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime, date, timedelta
-from pprint import pprint
+from datetime import date
 import apiclient
 from oauth2client.service_account import ServiceAccountCredentials
 from httplib2 import Http
 
+# Ref: https://developers.google.com/google-apps/calendar/quickstart/python
 
 # Authentication.
-# Ref: https://developers.google.com/google-apps/calendar/quickstart/python
 json_file = 'secret/yoheim-labo-3178ee6fa5bf.json'
 scopes = ['https://www.googleapis.com/auth/calendar.readonly']
 credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file, scopes=scopes)
 http_auth = credentials.authorize(Http())
 service = apiclient.discovery.build("calendar", "v3", http=http_auth)
 
-
-
-"""
-eventsResult = service.events().list(
-        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
-        orderBy='startTime').execute()
-    events = eventsResult.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
-"""
-
-# now = datetime.utcnow().isoformat() + 'Z'
-# print(now)
 
 calendar_id = "ja.japanese#holiday@group.v.calendar.google.com"
 dtfrom      = date(year=2016, month=1, day=1).isoformat() + "T00:00:00.000000Z"
@@ -44,9 +26,28 @@ events_results = service.events().list(
         singleEvents = True,
         orderBy = "startTime"
     ).execute()
-# pprint(events_results)
+
 events = events_results.get('items', [])
 for event in events:
     print("%s\t%s" % (event["start"]["date"], event["summary"]))
+"""
+2016-01-01  元日
+2016-01-11  成人の日
+2016-02-11  建国記念の日
+2016-03-20  春分の日
+2016-03-21  春分の日 振替休日
+2016-04-29  昭和の日
+2016-05-03  憲法記念日
+2016-05-04  みどりの日
+2016-05-05  こどもの日
+2016-07-18  海の日
+2016-08-11  山の日
+2016-09-19  敬老の日
+2016-09-22  秋分の日
+2016-10-10  体育の日
+2016-11-03  文化の日
+2016-11-23  勤労感謝の日
+2016-12-23  天皇誕生日
+"""
 
 
