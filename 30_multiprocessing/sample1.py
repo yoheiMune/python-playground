c5df20d305e36122e3ffafa9ea3babca6ad73143
Yohei Munesada <y.munesada@gmail.com>
2017-05-31 19:45:10 +0900

    Add descriptions.

--- 30_multiprocessing/sample1.py
+++ 30_multiprocessing/sample1.py
@@ -6,6 +6,7 @@
 """
 import time
 import os
+import sys
 
 # Process クラス
 ###################
@@ -18,9 +19,12 @@ def f1(name):
     print("Good morning", name)
 
 # if __name__ == "__main__":
+#     # Processを作成します
 #     p = Process(target=f1, args=("Bob",))
+#     # 開始します
 #     p.start()
 #     print("Process started.")
+#     # サブプロセス終了まで待ちます
 #     p.join()
 #     print("Process joined.")
 
@@ -33,14 +37,19 @@ from multiprocessing import Queue
 
 def f2(q):
     time.sleep(3)
+    # 3秒後に、キューに値を渡します.
     q.put([42, None, "Hello"])
 
 # if __name__ == "__main__":
+#     # スレッド間でやり取りするためのキューを作成します.
 #     q = Queue()
+#     # キューを引数に渡して、サブプロセスを作成します.
 #     p = Process(target=f2, args=(q,))
+#     # サブプロセスを開始します.
 #     p.start()
-#     # q.get() できるまで待つ.
+#     # q.get()できるまで待ちます.
 #     print(q.get())
+#     # サブプロセス完了を待ちます.
 #     p.join()
 
 
@@ -49,17 +58,26 @@ from multiprocessing import Pipe
 
 def f3(conn):
     time.sleep(3)
-    conn.send([42, None, "Hello"])
+    # パイプにデータを送信します.
+    conn.send({ "age" : 30, "name" : "Yohei" })
+    # パイプをクローズします.
     conn.close()
     # conn.send([42, None, "Hello"])  # OSError: handle is closed
 
 # if __name__ == "__main__":
+#     # Pipeを生成します（デフォルトでは双方向にやり取りできるパイプ）
+#     # 双方向にやり取りできますが、両端で自由に読み書きしているとデータが壊れる可能性があるので、
+#     # 基本的にはどちらかを書き込み専用、どちらかを読み込み専用に扱います.
 #     parent_conn, child_conn = Pipe()
+#     # Pipeの片方の端を、サブプロセスに渡します.
 #     p = Process(target=f3, args=(child_conn,))
+#     # サブプロセスを開始します.
 #     p.start()
-#     # parent_conn.recv() できるまで待つ.
+#     # Pipeから値が取得できるまで待ちます.
 #     print(parent_conn.recv())
+#     # サブプロセスの終了を待ちます.
 #     p.join()
+
 
 
 # Lock を用いたプロセス間の同期
@@ -67,17 +85,20 @@ def f3(conn):
 from multiprocessing import Lock
 
 def f4(lock, i):
+    # ロックを取得します.
     lock.acquire()
+    # ロック中は、他のプロセスやスレッドがロックを取得できません（ロックが解放されるまで待つ）
     try:
         print('Hello', i)
     finally:
+        # ロックを解放します.
         lock.release()
 
 # if __name__ == "__main__":
+#     # ロックを作成します.
 #     lock = Lock()
 #     for num in range(10):
 #         Process(target=f4, args=(lock, num)).start()
-#     # print("aaa")
 
 
 # 状態の共有
@@ -92,32 +113,46 @@ def f5(n, a):
         a[i] *= -1
 
 # if __name__ == "__main__":
+#     # 共有メモリ（Value）を作成します.
 #     num = Value('d', 0.0)
+#     # 共有メモリ（Array）を作成します.
 #     arr = Array('i', range(10))
+#     # サブプロセスを作り、実行します.
 #     p = Process(target=f5, args=(num, arr))
 #     p.start()
 #     p.join()
+#     # 共有メモリ（Value）から値を取り出します
 #     print(num.value)
+#     # 共有メモリ（Array）から値を取り出します
 #     print(arr[:])
+
 
 # マネージャー（Manager）の利用
 from multiprocessing import Manager
 
 def f6(d, l):
+    # 辞書型に値を詰め込みます.
     d[1] = '1'
     d["2"] = 2
     d[0.25] = None
+    # 配列を操作します（ここでは逆順に）.
     l.reverse()
 
-# if __name__ == "__main__":
-#     with Manager() as manager:
-#         d = manager.dict()
-#         l = manager.list(range(10))
-#         p = Process(target=f6, args=(d,l))
-#         p.start()
-#         p.join()
-#         print(d)
-#         print(l)
+if __name__ == "__main__":
+    # マネージャーを生成します.
+    with Manager() as manager:
+        # マネージャーから辞書型を生成します.
+        d = manager.dict()
+        # マネージャーから配列を生成します.
+        l = manager.list(range(10))
+        # サブプロセスを作り実行します.
+        p = Process(target=f6, args=(d,l))
+        p.start()
+        p.join()
+        # 辞書からデータを取り出します.
+        print(d)
+        # 配列からデータを取り出します.
+        print(l)
 
 
 # ワーカープロセスのプール（Poolクラス）
@@ -128,37 +163,38 @@ def f7(x):
     return x*x
 
 if __name__ == "__main__":
-    # Start 4 worker processes.
+    # 4つのプロセスを開始します.
     with Pool(processes=4) as pool:
 
-        # print "[0, 1, 4,..., 81]"
+        # 0〜9の10個の値を、4つのプロセスで処理します.
         print(pool.map(f7, range(10)))
+        # print: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
-        # print same numbers in arbitrary order.
+        # ひとつ上と似ていますが、プロセスの実行順が不定になります.
         for i in pool.imap_unordered(f7, range(10)):
             print(i)
+            # print: 0, 1, 4, 16, 9, 25, 36, 49, 64, 81
 
-        # Evaluate "f7(20)" asynchronously.
-        res = pool.apply_async(f7, (20,))  # runs in *only* one process.
-        print(res.get(timeout=1))         # print "400"
+        # "f7(20)"という処理を非同期に実行します（利用するプロセスは1つのみ）
+        res = pool.apply_async(f7, (20,))
+        # 処理結果が返ってくるまで、最大1秒間待機します
+        print(res.get(timeout=1))
+        # print: 400
 
-        # launching multiple evaluations asynchronously *may* use more processes.
+        # 上記の仕組みを連続して呼ぶと、複数のプロセスで処理を行うことができます.
         multiple_results = [pool.apply_async(os.getpid, ()) for i in range(10)]
         results = [res.get(timeout=1) for res in multiple_results]
+        # 10個出力されます
         print(results)
-        print(set(results), len(set(results)))
+        # print : [37604, 37607, 37606, 37605, 37604, 37607, 37606, 37605, 37604, 37607]
 
-        # Timeout error.
+        # ただしユニークにすると4つのみ（=プールしたプロセス数）であることがわかります
+        print(set(results), len(set(results)))
+        # {37604, 37605, 37606, 37607} 4
+
+        # タイムアウトが発生する場合のサンプルです
         res = pool.apply_async(time.sleep, (10,))
         try:
             res.get(timeout=1)
         except TimeoutError as e:
             print("Timeout.....!!!", e)
-
-        # Work fine.
-        print("With句の中ではProcessは生きています.")
-
-    print("With句から出るとProcessは利用できなくなります.")
-
-
-
